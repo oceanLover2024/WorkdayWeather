4b601dc3b2cc9d02b44bab3fea88531adcb785a7
siao <contentment731@yahoo.com.tw>
2025-05-02 05:43:00 +0800

    clear console

--- docs/js/weatherNow.js
+++ docs/js/weatherNow.js
@@ -9,7 +9,7 @@ async function fetchNow() {
 }
 export async function getCurrentData(cityName) {
   const data = await fetchNow();
-  console.log(data);
+
   const cityData = data.find((city) => city.LocationName.includes(cityName));
   if (!cityData) {
     console.log("無此縣市資料");
