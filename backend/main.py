079f192e28827e76c144ee7de47c4aeca3e96294
Aya Ting <ayateiiii@gmail.com>
2025-05-04 20:38:46 +0800

    chore: update allow_origins

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(message.router)
 app.include_router(stream.router)
 
-origins=["*"]
+origins=["https://addfish904.github.io","https://ayating.github.io","https://oceanLover2024.github.io"]
 
 app.add_middleware(
     CORSMiddleware,
